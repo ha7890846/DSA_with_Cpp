2d95ff410b413f4194da88064d93c9fe9614f038
ha7890846 <ha7890846@gmail.com>
2023-10-04 21:03:04 +0530

    update some changes in new line

--- Demo.cpp
+++ Demo.cpp
@@ -4,7 +4,7 @@ int main(){
     for(int i =0;i<10;i++){
         cout<<"hello chalrie\n";
         for(int j=0;j<10;j++){
-            cout<<"hleo coder life.";
+            cout<<"hleo coder life\n.";
         }
 
     }
