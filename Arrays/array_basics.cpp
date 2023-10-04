0abe5d1a6322f54937fd6ed212ba8e42c492f0cb
ha7890846 <ha7890846@gmail.com>
2023-10-04 21:06:28 +0530

    new folder added named "arrays"

---
+++ Arrays/array_basics.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    for(int i =0;i<10;i++){
+        cout<<"hello chalrie\n";
+        for(int j=0;j<10;j++){
+            cout<<"hleo coder life\n.";
+        }
+
+    }
+    return 0;
+}
