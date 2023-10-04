1928257cff954e60ce92688cf7a1f76d91e94243
ha7890846 <ha7890846@gmail.com>
2023-10-04 21:01:42 +0530

    adding new printing line

--- Demo.cpp
+++ Demo.cpp
@@ -3,6 +3,9 @@ using namespace std;
 int main(){
     for(int i =0;i<10;i++){
         cout<<"hello chalrie\n";
+        for(int j=0;j<10;j++){
+            cout<<"hleo coder life.";
+        }
 
     }
     return 0;
