f5f8de5bec08a2c8ee6c7e5e148bf08de8c6ab95
ha7890846 <ha7890846@gmail.com>
2023-10-05 22:59:22 +0530

    Here we are adding 2d Array file

---
+++ Arrays/Multi-D_array.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //initialize 2d array...
+    int row=3;
+    int col=4;
+    int arr1[row][col]={{0,1,2,0},{3,4,5,6},{7,8,9,0}};
+    int arr2[row][col];
+    int count=0;
+    for(int i=0;i<row;i++){
+        for(int j=0;j<col;j++){
+            arr2[i][j]=count;
+            count++;
+        }
+    }
+    // we represents 2d as matrix....
+    // Traverse every element in array..
+    cout<<"Let's Print arr1:\n";
+    for(int i=0;i<row;i++){
+        for(int j=0;j<col;j++){
+            cout<<arr1[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    cout<<"Here we Print second arr2:\n";
+    for(int i=0;i<row;i++){
+        for(int j =0;j<col;j++){
+            cout<<arr2[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+
+
+    return 0;
+}
