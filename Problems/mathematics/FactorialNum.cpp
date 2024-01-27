228985cc7425ed52aec4539daafe55088111ef24
ha7890846 <ha7890846@gmail.com>
2024-01-27 10:49:39 +0530

    adding factorial of small Number

---
+++ Problems/mathematics/FactorialNum.cpp
@@ -0,0 +1,27 @@
+// program for factorial small number from 0 to 12..
+#include<iostream>
+using namespace std;
+long long facto(int number){
+    // Base Condition ....
+    if(number == 0 ){
+        return 1;
+    }
+    int ans = number*facto(number-1);
+    return ans;
+}
+int itr_facto(int num){
+    int ans = 1;
+    while(num){
+        ans = ans*num;
+        num--;
+    }
+    return ans;
+}
+int main(){
+    int n;
+    cout<<"Enter the number range(1-12):";
+    cin>>n;
+    cout<<"The Factorial of the number is( Recursive method ): "<<facto(n)<<endl;
+    cout<<"The Factorial of the Number is ( iterative method ): "<<itr_facto(n)<<endl;
+    return 0;
+}
