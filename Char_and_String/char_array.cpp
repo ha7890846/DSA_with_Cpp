bac5fedacd7060b8b901d98f856896c3ad4f0cc9
ha7890846 <ha7890846@gmail.com>
2024-02-07 01:18:58 +0530

    improverment in char and str section

--- Char_and_String/char_array.cpp
+++ Char_and_String/char_array.cpp
@@ -1,6 +1,13 @@
 #include<iostream>
 #include<string.h>
+#include<limits>
 using namespace std;
+void printString(char str[],int size){
+    for (int i=0;i<size;i++){
+        cout<<str[i];
+    }
+    cout<<endl;
+ }
 int main(){
     char name[]={'a','b','c','\0'};
     char string[]="chalri";
@@ -19,5 +26,34 @@ int main(){
     char str1[]="here is the output";
     char str2[]="is";
     cout<<"find str:"<<strstr(str1,str2);
+    // Some new thing to know..
+     // here '\0' is a termination key,,,,,,,,
+    char arr[]={'a','b', 'c','d','e','f','\0','s','r'};
+    int length =sizeof(arr)/sizeof(arr[0]);
+    cout<<"the length of arr is: "<<length<<endl<<"The char word: ";
+    printString(arr,length);
+    /// Multiple char into time ...
+    char brr[100];
+    cout<<"Enter your name: ";
+    cin>>brr;
+    cout<<"my name is:"<<brr<<endl;
+    int size_brr=0;
+    int j=0;
+    while(brr[j] !='\0'){
+        size_brr++;
+        j++;
+    }
+    cout<<"the size of brr is:"<<size_brr<<endl;
+    for(i=0;i<size_brr;i++){
+        cout<<"The index value ["<<i<<"] and the char is at index : "<<brr[i]<< endl;
+    }
+    // for escalate, termination char use gelint function....
+    char student[100];
+    cout<<"Enter the student name(use space or tab is allowed ): ";
+    // it will clear keyboard input buffer..... contains in <limits> header file
+    // including the new line character also
+    cin.ignore(numeric_limits<streamsize>::max(),'\n');    
+    cin.getline(student,50);
+    cout<<"the name you have entered is : "<<student<<endl;
     return 0;
 }
