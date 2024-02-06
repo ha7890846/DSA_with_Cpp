8638f26f28a12a8b32e294a1b2c94d1a50b3228e
ha7890846 <ha7890846@gmail.com>
2024-02-07 01:28:58 +0530

    changes in string

--- Char_and_String/string.cpp
+++ Char_and_String/string.cpp
@@ -1,9 +1,13 @@
 #include<iostream>
+#include<string>    // for string function.
 using namespace std;
 int main(){
 
-////// string predefined STl   .........
+// string predefined STl   .........
+    string s1 = "Hello coder";
+    cout<<"String S1: "<<s1<<endl;
     string name;
+    cout<<"Enter Your name: ";
     getline(cin,name);
     cout<<"Your name is : "<<name <<endl;
     return 0;  
