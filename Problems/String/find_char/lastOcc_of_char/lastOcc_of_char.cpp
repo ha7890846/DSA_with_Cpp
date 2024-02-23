87132e2fc55f4194ca97ef89c134ea8f322fa821
ha7890846 <ha7890846@gmail.com>
2024-02-23 21:43:18 +0530

    last and first occ of x

--- Problems/String/find_char/lastOcc_of_char/lastOcc_of_char.cpp
+++ Problems/String/find_char/lastOcc_of_char/lastOcc_of_char.cpp
@@ -6,11 +6,13 @@ int main()
 {
     string str = "Coding is easy is";
     string x = "is";
-    int found = str.rfind(x);
+    int rev_found = str.rfind(x);
+    int found = str.find(x);
     int i = 0;
     while (found != string::npos)
     {
-        cout << found;
+        cout <<"The first index of x is: "<< found<<endl;
+        cout<<"The last index of x: "<<rev_found<<endl;
         break;
     }
     return 0;
