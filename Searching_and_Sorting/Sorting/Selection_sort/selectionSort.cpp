a8bdddaea4135ce84bddcc9bb6423ac5f1e00baf
ha7890846 <ha7890846@gmail.com>
2024-02-29 19:26:03 +0530

    fix  selection sort

--- Searching_and_Sorting/Sorting/Selection_sort/selectionSort.cpp
+++ Searching_and_Sorting/Sorting/Selection_sort/selectionSort.cpp
@@ -2,26 +2,27 @@
 #include<vector>
 using namespace std;
 void selectionSort(vector<int>&arr){
-    for(int i=0;i<numbers.size()-1;i++){
+    for(int i=0;i<arr.size()-1;i++){
         int mini = i;
-        for(int j=i+1;j<numbers.size();j++){
-            if(numbers[j]<numbers[mini]){
+        for(int j=i+1;j<arr.size();j++){
+            if(arr[j]<arr[mini]){
                 mini = j;
             }
         }
-        swap(numbers[i],numbers[mini]);
+        swap(arr[i],arr[mini]);
     }
 }
 int main(){
-    vector<int>numbers{6,8,11,10,9,4,2,7,5,1};
+    vector<int>numbers{6,8,11,10,1,6,8,9,4,2,7,5,1};
     cout<<"\t\t\t****Selection Sort******"<<endl;
     // it is a unstable sorting algo 
-    cout<<"\nThe Original Array is: ";
+    cout<<"The Original Array is: ";
     for(auto v:numbers){
         cout<<v<<", ";
     }
     cout<<endl;
-    cout<<"The Original Array is: ";
+    selectionSort(numbers);
+    cout<<"\nThe Sorted Array is: ";
     for(auto v:numbers){
         cout<<v<<", ";
     }
