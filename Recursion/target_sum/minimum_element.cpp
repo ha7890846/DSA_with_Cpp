471cf81289d2003217670eaf27e95ae0cc493faa
ha7890846 <ha7890846@gmail.com>
2024-03-09 22:08:57 +0530

    adding max and min count of element to reach target

--- Recursion/target_sum/minimum_element.cpp
+++ Recursion/target_sum/minimum_element.cpp
@@ -18,10 +18,39 @@ int countMinEle(vector<int>& arr, int target){
     }
     return mini;
 }
+int countMaxEle(vector<int>&arr, int target){
+    if(target == 0){
+        return 0;
+    }
+    if(target < 0){
+        return INT_MIN;
+    }
+    int maxi = INT_MIN;
+    for(int i = 0;i<arr.size();i++){
+        int ans = countMaxEle(arr,target-arr[i]);
+        if(ans != INT_MIN){
+            maxi = max(ans+1,maxi);
+        }
+
+    }
+    return maxi;
+}
 int main(){
-    vector<int>arr{5,4};
+    vector<int>arr{2,4};
     int target = 19;
-    cout<<"Minimum num of Element :"<<countMinEle(arr,target)<<endl;
-    // cout<<"Total no of ways to reach: "<<totalWays(arr,target)<<endl;
+    int maxCount = countMaxEle(arr,target);
+    int minCount = countMinEle(arr,target);
+    cout<<"Minimum num of Element :";
+    if(minCount != INT_MAX){
+        cout<<minCount<<endl;
+    }else{
+        cout<<0<<endl;
+    }
+    cout<<"Max num of Element : ";
+    if(maxCount != INT_MIN){
+        cout<<maxCount<<endl;
+    }else{
+        cout<<0<<endl;
+    }
     return 0;
 }
