6f59a58777795a83648f3aed5b431a5817de970e
ha7890846 <ha7890846@gmail.com>
2024-05-14 15:20:43 +0530

    solve remove_duplicates problem

---
+++ LINKED_LIST/Circular_LL/list_basics.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node* next;
+
+    Node(){
+        this->data = 0;
+        this->next = NULL;
+    }
+    Node(int data){
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+
+  
+
+int main(){
+
+    return 0;
+}
