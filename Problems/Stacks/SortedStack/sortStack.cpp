38aef07f6a83e6732e070002a6e8006b60634760
ha7890846 <ha7890846@gmail.com>
2024-08-05 22:05:00 +0530

    Sort Stack using Recursion

--- Problems/Stacks/SortedStack/sortStack.cpp
+++ Problems/Stacks/SortedStack/sortStack.cpp
@@ -1,25 +1,64 @@
-#include<iostream>
-#include<stack>
+#include <iostream>
+#include <stack>
 using namespace std;
-// This is not By Ref ...
-void printStack(stack<int>s){
-    while(!s.empty()){
-        cout<<s.top()<<", ";
+
+void printStack(stack<int> s) {
+    while (!s.empty()) {
+        cout << s.top() << ", ";
         s.pop();
     }
+    cout << endl;
 }
 
-int main(){
-    stack<int>s;
-    s.push(70); 
-    s.push(15); 
-    s.push(12); 
-    s.push(21); 
-    s.push(25); 
-    s.push(13); 
-    s.push(17); 
-    s.push(18); 
-    cout<<"Printing the Stack: ";
+void insertSorted(stack<int>& s, int value) {
+    // Base case: stack is empty or top element is less than value
+    if (s.empty() || s.top() < value) {
+        s.push(value);
+        return;
+    }
+
+    // Remove the top element
+    int temp = s.top();
+    s.pop();
+
+    // Recur for the remaining elements in the stack
+    insertSorted(s, value);
+
+    // Push the top element back
+    s.push(temp);
+}
+
+void sortStack(stack<int>& s) {
+    // Base case: stack is empty
+    if (s.empty()) {
+        return;
+    }
+    // Remove the top element
+    int value = s.top();
+    s.pop();
+
+    // Recur for the remaining elements in the stack
+    sortStack(s);
+
+    // Insert the top element back in sorted order
+    insertSorted(s, value);
+}
+
+int main() {
+    stack<int> s;
+    s.push(30);
+    s.push(10);
+    s.push(50);
+    s.push(20);
+    s.push(40);
+
+    cout << "Original stack: ";
     printStack(s);
+
+    sortStack(s);
+
+    cout << "Sorted stack: ";
+    printStack(s);
+
     return 0;
 }
