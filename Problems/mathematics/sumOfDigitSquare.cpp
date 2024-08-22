fd5356cd2d8a9251bde6e1f3b9313c7ab248435e
ha7890846 <ha7890846@gmail.com>
2024-08-22 22:44:42 +0530

    fixing bugs

--- Problems/mathematics/sumOfDigitSquare.cpp
+++ Problems/mathematics/sumOfDigitSquare.cpp
@@ -3,23 +3,22 @@ using namespace std;
 int result(int num,int& ans){
     if((num / 10) <= 0 ){
         ans += num*num;
-        cout<<" upper ans: "<<ans<<endl;
         return ans;
     }
     int digit = num %10;
     int remains = num/10;
     result(remains,ans);    
     ans += digit*digit;
-    cout<<"lower ans: "<<ans<<endl;
     return ans;
 }
 int main(){
     int num ;
+    cout<<"Enter a Number: ";
     cin>>num;
     while(num/10){
         int ans = 0;
         num = result(num,ans);
     }
-    cout<<num*num<<endl;
+    cout<<"The answer is: "<<num*num<<endl;
     return 0;
 }
